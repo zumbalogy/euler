b8370637a1925edd2d446ea7118f8342f1685515
zumbalogy <intrapostal@gmail.com>
2014-02-24 19:54:06 -0500

    clojure logic thought of, just syntax needs to be looked up

--- p3/p3.clj
+++ p3/p3.clj
@@ -1,3 +1,33 @@
 ;; The prime factors of 13195 are 5, 7, 13 and 29.
 
 ;; What is the largest prime factor of the number 600851475143 ?
+
+
+(+ 1 2 3)
+
+
+
+(mod 11 5)
+
+
+(set (range 0 1000 5))
+(range 0 100 5)
+
+(def start 13195)
+
+(def up_to (range 1 start))
+
+;(for [x (flatten foo) :while (<= x 5)] x)
+
+(mod start 29)
+
+(== (mod start 29) 0)
+
+(for [x up_to :when (== (mod start x) 0)] x)
+
+;; just go over the output of the above and make sure there (same method) has the highest value (or reduction of) zero, then return that one
+
+
+
+
+
