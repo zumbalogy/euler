5dda45e2e5dcb198a78590eb2aa449e57be556dd
zumbalogy <intrapostal@gmail.com>
2014-03-06 21:56:07 -0500

    comments

--- p5/p5.rb
+++ p5/p5.rb
@@ -10,14 +10,14 @@ Benchmark.bmbm do |bm|
         divisible = false
 
         until divisible == true
-            start += 2520  
-            if start % 380 == 0 &&
+            start += 2520 #iterating by this works, but is kinda cheating
+            if start % 380 == 0 &&  #these are some eyeballed lcms for 20-10
                 start % 323 == 0 &&
                 start % 143 == 0 &&
                 start % 63 == 0 &&
                 start % 60 == 0 &&
                 start % 16 == 0 &&
-                start % 12 == 0 # 5 3 2 
+                start % 12 == 0
                 divisible = true
             end
         end
@@ -58,47 +58,49 @@ Benchmark.bmbm do |bm|
     end
     bm.report('non recursive non function') do
         total = 20
-        (10..20).each do |i|
+        (11..19).each do |i|
             total = total.lcm(i) 
         end
     end
 end
 
-# start = 0
-# divisible = false
+###################################################
 
-# until divisible == true
-#     start += 2520  
-#     if start % 380 == 0 &&
-#         start % 323 == 0 &&
-#         start % 143 == 0 &&
-#         start % 63 == 0 &&
-#         start % 60 == 0 &&
-#         start % 16 == 0 &&
-#         start % 12 == 0 # 5 3 2 
-#         divisible = true
-#     end
-# end
+start = 0
+divisible = false
 
-# p start
+until divisible == true
+    start += 2520  
+    if start % 380 == 0 &&
+        start % 323 == 0 &&
+        start % 143 == 0 &&
+        start % 63 == 0 &&
+        start % 60 == 0 &&
+        start % 16 == 0 &&
+        start % 12 == 0 # 5 3 2 
+        divisible = true
+    end
+end
+
+p start
 
 # 232_792_560
 
 ####################################################################
 
-# def gcd(a, b)
-#     b == 0 ? a : gcd(b, a % b)
-# end
+def gcd(a, b)
+    b == 0 ? a : gcd(b, a % b)
+end
 
-# def lcm(a, b)
-#     (a * b) / gcd(a, b)
-# end
+def lcm(a, b)
+    (a * b) / gcd(a, b)
+end
 
-# def recursive(a, b=a-1, c=a/2)
-#         b > c ? recursive(lcm(a,b), b-1, c) : a
-# end
+def recursive(a, b=a-1, c=a/2)
+        b > c ? recursive(lcm(a,b), b-1, c) : a
+end
 
-# puts recursive(20)
+puts recursive(20)
 
 #################################################################
 
@@ -119,3 +121,29 @@ def nonrecursive(a)
 end
 
 puts nonrecursive(20)
+
+
+#####################################################
+total = 20
+(11..19).each do |i|
+    total = total.lcm(i) 
+end
+
+p total
+
+###################################################
+
+# Rehearsal --------------------------------------------------------------
+# dumb checking                0.000000   0.000000   0.000000 (  0.009043)
+# eulid own fuctions           0.000000   0.000000   0.000000 (  0.000024)
+# eulid ruby built in          0.000000   0.000000   0.000000 (  0.000020)
+# non recursive function       0.000000   0.000000   0.000000 (  0.000016)
+# non recursive non function   0.000000   0.000000   0.000000 (  0.000008)
+# ----------------------------------------------------- total: 0.000000sec
+
+#                                  user     system      total        real
+# dumb checking                0.010000   0.000000   0.010000 (  0.008780)
+# eulid own fuctions           0.000000   0.000000   0.000000 (  0.000032)
+# eulid ruby built in          0.000000   0.000000   0.000000 (  0.000020)
+# non recursive function       0.000000   0.000000   0.000000 (  0.000021)
+# non recursive non function   0.000000   0.000000   0.000000 (  0.000010)
