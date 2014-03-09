ebf04c7281d0ad7755e033145221350bc5246ba7
zumbalogy <intrapostal@gmail.com>
2014-03-09 17:36:23 -0400

    p9 js done

---
+++ p9/p9.js
@@ -0,0 +1,21 @@
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+// a^2 + b^2 = c^2
+
+// For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+
+
+loop:
+for (var a = 1; a < 1000; a++){
+    for (var b = 1; b < 1000-a; b++){
+        var c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2))
+        if (a+b+c == 1000){
+            console.log(a*b*c)
+            break loop // or giv empty return. or this would be a function
+        }
+    }
+}
+
+// 31875000
