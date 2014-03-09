9ab724093c4d141684561aa6f9ae6e9cec78b7c1
zumbalogy <intrapostal@gmail.com>
2014-03-08 23:22:20 -0500

    p12 in rb solved i think, but too slow to really check

--- p10/p10.rb
+++ p10/p10.rb
@@ -3,3 +3,18 @@
 # Find the sum of all the primes below two million.
 
 
+require 'prime'
+
+
+a = Prime.new
+
+sum = 0
+save = 0
+
+while save < 2_000_000
+    sum += save
+    save = a.next
+end
+
+puts sum
+# 142913828922
