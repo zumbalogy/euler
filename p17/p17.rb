7b2ac6e56d1e6f737b8b2a5b5e09c3d2b6fbab56
zumbalogy <intrapostal@gmail.com>
2014-03-14 09:28:51 -0400

    rb 17 closer, no cigar

--- p17/p17.rb
+++ p17/p17.rb
@@ -5,20 +5,37 @@
 # NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
 one_nine = 'onetwothreefourfivesixseveneightnine'.length
+# 36
 
 ten_nineteen = 'teneleventwelvethirfourfifsixseveneightnine'.length + ('teen'.length * 7)
+# 71
 
 twenty_ninty = 'twenthirfourfifsixseveneighnine'.length + ('ty'.length * 8)
+# 47
 
 sum_99 = one_nine + ten_nineteen + twenty_ninty + (one_nine * 8)
+# 442
 
-hundreds = one_nine + ('hundred'.length * 9) + ('and'.length * 8)
+####################################################
+
+hundred = one_nine + ('hundred'.length * 9)
+# 99
+
+hundred_and = (one_nine * 99) + ('hundredand'.length * 9 * 99)
+# 12474
+
+hundreds = hundred + hundred_and
+# 12573
+
+####################################################
 
 sum_999 = hundreds + (sum_99 * 10)
+# 16993
 
 one_thousand = 'onethousand'.length
+# 11
 
 total = sum_999 + one_thousand
 
 puts total
-# 4554
+# 17004
