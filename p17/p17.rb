efae2389ac0d7ebd3b15fc328d8650c06f6dba73
zumbalogy <intrapostal@gmail.com>
2014-03-14 09:09:33 -0400

    rb 17 possible solution

--- p17/p17.rb
+++ p17/p17.rb
@@ -2,6 +2,23 @@
 
 # If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 
-
 # NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
+one_nine = 'onetwothreefourfivesixseveneightnine'.length
+
+ten_nineteen = 'teneleventwelvethirfourfifsixseveneightnine'.length + ('teen'.length * 7)
+
+twenty_ninty = 'twenthirfourfifsixseveneighnine'.length + ('ty'.length * 8)
+
+sum_99 = one_nine + ten_nineteen + twenty_ninty + (one_nine * 8)
+
+hundreds = one_nine + ('hundred'.length * 9) + ('and'.length * 8)
+
+sum_999 = hundreds + (sum_99 * 10)
+
+one_thousand = 'onethousand'.length
+
+total = sum_999 + one_thousand
+
+puts total
+# 4554
