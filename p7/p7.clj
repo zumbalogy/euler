415585e89f4c3f5643b14ff3fd40869e9248bb62
zumbalogy <intrapostal@gmail.com>
2014-04-08 19:30:43 -0400

    scratchpad clojure

--- p7/p7.clj
+++ p7/p7.clj
@@ -1,1 +1,13 @@
 ;; What is the 10 001st prime number? (not including one)
+
+
+(defn is_prime [input]
+  (== (for [x (range 1 (/ input 2))
+            :when (== (mod input x) 0)] x) 1 ) )
+;; go to sqaure of the input
+
+
+(def x 4)
+
+(is_prime x)
+
