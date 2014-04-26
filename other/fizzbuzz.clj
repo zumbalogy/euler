cf69d0c271f627dfaf96cf903a33c7b9939570a7
zumbalogy <intrapostal@gmail.com>
2014-04-26 19:53:28 -0400

    fizzbuzz in clojure

--- other/fizzbuzz.clj
+++ other/fizzbuzz.clj
@@ -1,4 +1,11 @@
 
-(defn fb [x] )
+(defn fb [x] 
+    (if (= 0 (mod x 15))
+        "Fizzbuzz"
+        (if (= 0 (mod x 3))
+            "Buzz"
+            (if (= 0 (mod x 5))
+                "Fizz"
+                x))))
 
-(map - (vec (take 100 (range))))
+(map fb (vec (range 1 100)))
