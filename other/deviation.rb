6de168f06f1808ef532afc5c354f2eee220ee26f
zumbalogy <intrapostal@gmail.com>
2014-04-27 13:02:48 -0400

    comments

--- other/deviation.rb
+++ other/deviation.rb
@@ -1,24 +1,28 @@
-# https://gist.github.com/matthewrudy/8677812
+=begin
 
-# Given an array of integer elements and an integer d please consider all the sequences of d consecutive elements in the array. For each sequence we compute the difference between the maximum and the minimum value of the elements in that sequence and name it the deviation.
+https://gist.github.com/matthewrudy/8677812
 
-# Your task is to;
+Given an array of integer elements and an integer d please consider all the sequences of d consecutive elements in the array. For each sequence we compute the difference between the maximum and the minimum value of the elements in that sequence and name it the deviation.
 
-# write a function that computes the maximum value among the deviations of all the sequences considered above
-# print the value the standard output (stdout)
-# Note that your function will receive the following arguments:
+Your task is to;
 
-# v which is the array of integers
-# d which is an integer value giving the length of the sequences
+write a function that computes the maximum value among the deviations of all the sequences considered above
+print the value the standard output (stdout)
+Note that your function will receive the following arguments:
 
-# the array will contain up to 100,000 elements all the elements in the array are integer numbers in the following range: [1, 231 -1] the value of d will not exceed the length of the given array
+v which is the array of integers
+d which is an integer value giving the length of the sequences
 
-# Input:
-# v: 6, 9, 4, 7, 4, 1
-# d: 3
+the array will contain up to 100,000 elements all the elements in the array are integer numbers in the following range: [1, 231 -1] the value of d will not exceed the length of the given array
 
-# Output:
-# 6
+Input:
+v: 6, 9, 4, 7, 4, 1
+d: 3
+
+Output:
+6
+
+=end
 
 
 def deviation array, amount_input
--- other/fizzbuzz.clj
+++ other/fizzbuzz.clj
@@ -1,4 +1,3 @@
-
 (defn fb [x] 
     (if (= 0 (mod x 15))
         "Fizzbuzz"
@@ -7,5 +6,13 @@
             (if (= 0 (mod x 5))
                 "Fizz"
                 x))))
+
+(defn fb2 [x]
+    (cond
+        (= 0 (mod x 15)) "Fizzbuzz"
+        (= 0 (mod x 3)) "Buzz"
+        (= 0 (mod x 5)) "Fizz"
+        :else x))
+
 
 (map fb (vec (range 1 100)))
