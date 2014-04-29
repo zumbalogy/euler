0667683da4b4d9e1ba75c256d0d0606569b3bd93
zumbalogy <intrapostal@gmail.com>
2014-04-29 12:41:35 -0400

    qucksort in js

---
+++ other/sorts/quick.js
@@ -0,0 +1,10 @@
+function qs(array){
+    if (array.length <= 1){
+        return array
+    }
+    var pivot  = array[0]
+    var bottom = array.filter(function(x){return x < pivot})
+    var top    = array.filter(function(x){return x > pivot})
+    var middle = array.filter(function(x){return x == pivot})
+    return qs(bottom).concat(middle.concat(qs(top)))
+}
