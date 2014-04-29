b5c7fccf037217669f270a4ab33e86fb2ffa76e5
zumbalogy <intrapostal@gmail.com>
2014-04-29 13:42:12 -0400

    clean

--- other/sorts/quick.js
+++ other/sorts/quick.js
@@ -1,7 +1,5 @@
 function qs(array){
-    if (array.length <= 1){
-        return array
-    }
+    if (array.length <= 1){return array}
     var pivot  = array[0]
     var bottom = array.filter(function(x){return x < pivot})
     var middle = array.filter(function(x){return x == pivot})
