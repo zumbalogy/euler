84f2e32fa7b73884e5665e5b289c8ca073af2897
zumbalogy <intrapostal@gmail.com>
2014-05-03 00:59:21 -0400

    mergesort getting better, still errors

--- other/sorts/merge.js
+++ other/sorts/merge.js
@@ -1,15 +1,17 @@
 function split(array){
     var output = []
-    while (array.length > 0){
-        var foo = sort_pair([array.shift(), array.shift()])
-        output.push(foo)
+    while (array.length > 1){
+        output.push(sort_pair([array.shift(), array.shift()]))
+    }
+    if (array.length == 1){
+        output.push([array.shift()])
     }
     return output
 }
 
 function sort_pair(array){
     array = array.filter(function(x){
-        if (x) {return x}
+        if (typeof(x) == 'number') {return true}
     })
     if (array.length > 1 && array[0] > array[1]) {
         return [array[1], array[0]]
@@ -18,20 +20,39 @@ function sort_pair(array){
 }
 
 function merge(array1, array2){
-    var foo = array1.shift()
-    var bar = array2.shift()
     var output = []
-    while (array1.length > 0 && array2.length > 0) {
+    var save;
+    if (array1 && array2) {
+        var foo = array1.shift()
+        var bar = array2.shift()
+    }
+    while (array1 && array2 && array1.length > 0 && array2.length > 0) {
         if (foo < bar){
             output.push(foo)
             foo = array1.shift()
+            var save = foo;
         } else {
             output.push(bar)
             bar = array2.shift()
+            var save = bar;
         }
     }
+    output.push(save)
+    while (array1 && array1.length > 0){
+        output.push(array1.shift())
+    }
+    while (array2 && array2.length > 0){
+        output.push(array2.shift())
+    }
+    return output
 }
 
 function mergeSort(array){
-
+    var output = split(array);
+    var count = output.length;
+    for (var i = 0; i < count; i++){
+        output[i] = merge(output[i], output[i+1])
+        output.splice(i+1, 1)
+    }
+    return output
 }
