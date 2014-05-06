ed75b96c84e2314a900801b6483c58b3bc54242f
zumbalogy <intrapostal@gmail.com>
2014-05-06 00:08:14 -0400

    additional solution to p4 ruby

--- p4/p4.rb
+++ p4/p4.rb
@@ -6,7 +6,7 @@ output = 0
 (100..999).each do |foo|
     (foo..999).each do |bar|
         foobar = foo * bar
-        if foobar > output && foobar.to_s.reverse == foobar.to_s
+        if foobar > output && foobar.to_s.reverse.to_i == foobar
             output = foobar
         end
     end
@@ -16,3 +16,31 @@ end
 puts output
 # 906609
 # 913 * 993
+
+
+###########################################
+
+# could step down from 998001 until hit palindromes, and then see if they
+# factor into 2 3digit nums
+
+start = 999 * 999
+
+def is_pal? input
+    input.to_s.reverse.to_i == input
+end
+
+def facts_to_2_3s input
+    (100..999).each do |i|
+        if input % i == 0 && (input / i) < 999 
+            return true
+        end
+    end
+    return false
+end
+
+until is_pal?(start) && facts_to_2_3s(start)
+    start -= 1
+end
+
+p start
+# 906609
