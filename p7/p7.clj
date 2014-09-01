2140e6ad96b0cfa638d6b4132def968eb81ef888
zumbalogy <intrapostal@gmail.com>
2014-09-01 19:29:50 -0400

    working is_prime function for p7 clojure

--- p7/p7.clj
+++ p7/p7.clj
@@ -1,13 +1,13 @@
 ;; What is the 10 001st prime number? (not including one)
 
-
 (defn is_prime [input]
-  (== (for [x (range 1 (/ input 2))
-            :when (== (mod input x) 0)] x) 1 ) )
-;; go to sqaure of the input
+  (= [1] (for [x (range 1 (Math/sqrt input))
+    :when (== (mod input x) 0)] x)))
 
 
 (def x 4)
 
 (is_prime x)
+
+
 
