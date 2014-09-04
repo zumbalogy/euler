245273ca0d545be3a055e9f416cfd145caa73779
zumbalogy <intrapostal@gmail.com>
2014-09-03 22:38:27 -0400

    p4 clojure done

--- p4/p4.clj
+++ p4/p4.clj
@@ -1,3 +1,28 @@
 ; look for the largest palindome number (9009) that
 ; is the product of 2 3-digit numbers.
 
+(use '[clojure.string :only (join split)])
+
+(defn is_pal [input]
+  (= (str input) (join (reverse (str input)))))
+
+(def best (atom 0))
+
+(defn loop_check [input]
+  (loop [x 999]
+    (when (< 0 x)
+      (if (and
+          (is_pal (* x input))
+          (< @best (* x input)))
+        (reset! best (* x input)))
+      (recur (dec x)))))
+
+(loop [x 999]
+  (when (< 0 x)
+    (loop_check x)
+    (recur (dec x))))
+
+(print @best)
+
+; 906609
+; from 913 993
