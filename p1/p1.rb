dc7a8a7071955b0a29744c02733c80205cb0f540
zumbalogy <intrapostal@gmail.com>
2014-09-04 23:50:45 -0400

    clean

--- p1/p1.rb
+++ p1/p1.rb
@@ -3,17 +3,6 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-total = 0
-
-total += (0..1000).step(3).reduce(:+)
-total += (0..1000).step(5).reduce(:+)
-total -= (0..1000).step(15).reduce(:+)
-
-p total
-# =>  233168
-
-############################################
-
 def sum_series input
     ((1000 / input) / 2.0) * (input + (1000 - (1000 % input)))
 end
