e91c6beea4a82835ae3afe685d92715eebca9018
zumbalogy <intrapostal@gmail.com>
2014-09-05 21:59:19 -0400

    clean

--- p16/p16.rb
+++ p16/p16.rb
@@ -2,12 +2,9 @@
 
 # What is the sum of the digits of the number 2^1000?
 
-
 puts (2 ** 1_000).to_s.split('').map {|i| i.to_i}.reduce(:+)
 
-
 puts (2 ** 1_000).to_s.chars.inject(0) {|result, elem| result + elem.to_i}
-
 
 # 1366
 # not the most clever solution, but straightforward and short.
