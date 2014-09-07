8c403dc18482c7b4419d22fff30fff08924455b5
zumbalogy <intrapostal@gmail.com>
2014-09-07 18:29:07 -0400

    refactor p20

--- p20/p20.rb
+++ p20/p20.rb
@@ -5,9 +5,14 @@
 
 # Find the sum of the digits in the number 100!
 
-total = (2..100).inject(1) {|a,b| a * b}.to_s.chars.inject(0) {|a, b| a + b.to_i}
+def sum_digits input
+  input.to_s.chars.reduce(0) { |a, b| a + b.to_i }
+end
+
+total = sum_digits (2..100).reduce(1, &:*)
 
 puts total
 # 648
 
-p (2..100).inject(1) {|a,b| a * b}
+p (2..100).reduce(1, &:*)
+p (2..100).reduce(1, &:*).to_s.chars.reduce(0) {|a, b| a + b.to_i}
