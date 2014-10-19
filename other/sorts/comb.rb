fdcdfe0cf4d20a80745742be257da3f744845a11
zumbalogy <intrapostal@gmail.com>
2014-10-19 13:12:47 -0400

    indentaion in ruby comb sort

--- other/sorts/comb.rb
+++ other/sorts/comb.rb
@@ -1,16 +1,16 @@
 def comb_sort array
-    array.count.times do |pass|
-        foo = array.count - pass
-        array.each_index do |index|
-            if array[index + foo]
-                a = array[index]
-                b = array[index + foo]
-                if a > b
-                    array[index] = b
-                    array[index + foo] = a
-                end
-            end
+  array.count.times do |pass|
+    foo = array.count - pass
+    array.each_index do |index|
+      if array[index + foo]
+        a = array[index]
+        b = array[index + foo]
+        if a > b
+          array[index] = b
+          array[index + foo] = a
         end
+      end
     end
-    array
+  end
+  array
 end
