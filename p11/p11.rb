4bb5ff1d34e60218163468c9f0aaf9f7336f1098
zumbalogy <intrapostal@gmail.com>
2014-11-19 22:16:48 -0500

    p11 ruby work

--- p11/p11.rb
+++ p11/p11.rb
@@ -1,6 +1,5 @@
 # What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
 
-
 grid = [[ 8, 2,22,97,38,15, 0,40, 0,75, 4, 5, 7,78,52,12,50,77,91, 8],
         [49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48, 4,56,62, 0],
         [81,49,31,73,55,79,14,29,93,71,40,67,53,88,30, 3,49,13,36,65],
@@ -24,26 +23,28 @@ grid = [[ 8, 2,22,97,38,15, 0,40, 0,75, 4, 5, 7,78,52,12,50,77,91, 8],
 
 best = 0
 
+limit = grid.length - 3
+
 grid.each_with_index do |row, row_num|
   row.each_with_index do |cell, cell_num|
 
-    if cell_num < row.length - 3
+    if cell_num < limit
       right = cell * row[cell_num + 1] * row[cell_num + 2] * row[cell_num + 3]
     end
 
-    if row_num < grid.length - 3
+    if row_num < limit
       down = cell * grid[row_num + 1][cell_num] * grid[row_num + 2][cell_num] * grid[row_num + 3][cell_num]
     end
 
-    if cell_num < row.length - 3 && row_num < grid.length - 3
-      diag = cell * grid[row_num + 1][cell_num + 1] * grid[row_num + 2][cell_num + 2] * grid[row_num + 3][cell_num + 3]
+    if cell_num < limit && row_num < limit
+      diag_r = cell * grid[row_num + 1][cell_num + 1] * grid[row_num + 2][cell_num + 2] * grid[row_num + 3][cell_num + 3]
     end
 
-    if cell_num > 3 && row_num < grid.length - 3
-      diag2 = cell * grid[row_num + 1][cell_num - 1] * grid[row_num + 2][cell_num - 2] * grid[row_num + 3][cell_num - 3]
+    if cell_num > 3 && row_num < limit
+      diag_l = cell * grid[row_num + 1][cell_num - 1] * grid[row_num + 2][cell_num - 2] * grid[row_num + 3][cell_num - 3]
     end
 
-    best = [best, right, down, diag, diag2].compact.max
+    best = [best, right, down, diag_r, diag_l].compact.max
 
   end
 end
