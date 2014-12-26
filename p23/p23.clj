9e9b8d84c4b5d70b42e7fd5b0afa4bd7316c0b2b
zumbalogy <intrapostal@gmail.com>
2014-12-26 13:36:29 -0500

    clean

--- p23/p23.clj
+++ p23/p23.clj
@@ -1,6 +1,4 @@
-; A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
-
-; A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+; A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number. A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
 
 ; As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28_123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
 
@@ -10,17 +8,17 @@
 
 (use 'clojure.set)
 
-(defn take_divs [n]
+(defn take_factors [n]
   (filter #(zero? (mod n %)) (range 1 n)))
 
-(defn sum_divs [n]
-  (reduce + (take_divs n)))
+(defn sum_factors [n]
+  (reduce + (take_factors n)))
 
 (def abundants
-  (filter #(< % (sum_divs %)) (range)))
+  (filter #(< % (sum_factors %)) (range)))
 
 (defn add_pairs [input]
-  (set (for [x input y input] (+ x y)))) ; this is doing everything twice. only needs to do it for Ys after X
+  (set (for [x input y input] (+ x y))))
 
 (def my_combos
   (add_pairs (take-while #(> 28123 %) abundants)))
--- p23/p23.rb
+++ p23/p23.rb
@@ -1,6 +1,4 @@
-# A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
-
-# A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+# A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number. A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
 
 # As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28_123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
 
@@ -8,31 +6,23 @@
 
 ########################
 
-# The first odd abundant number not divisible by 5 is 81_081
-
-# maybe i should try starting at 12 and the number. 12 + first abundant under input a hit?
-# is next abundant up from 12 and next abundant underneath input a hit? etc
-
-
-
-
 def abundant? input
-    a = 0
-	(2..Math.sqrt(input)).each do |foo|
-		a += (foo + (input / foo)) if (input % foo).zero?
-		return true if a > input
-	end
-	return false
+  a = 0
+  (2..Math.sqrt(input)).each do |foo|
+    a += (foo + (input / foo)) if (input % foo).zero?
+    return true if a > input
+  end
+  false
 end
 
 def made_from_abundant? input
-    return false if input.odd? && input % 5 == 0
-    (12..(input/2)).each do |first|
-        if abundant?(first) && abundant?(input - first)
-            return true
-        end
+  return false if input.odd? && input % 5 == 0
+  (12..(input/2)).each do |first|
+    if abundant?(first) && abundant?(input - first)
+      return true
     end
-    return false
+  end
+  false
 end
 
 puts abundant? 11
