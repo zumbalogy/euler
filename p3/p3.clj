c778c1ff3e27682258b1b6c0a5fc5f38c4ea50fa
zumbalogy <intrapostal@gmail.com>
2015-01-10 17:47:54 -0500

    clean

--- p3/p3.clj
+++ p3/p3.clj
@@ -1,7 +1,5 @@
-;; The prime factors of 13195 are 5, 7, 13 and 29.
-
-;; What is the largest prime factor of the number 600851475143 ?
-
+; The prime factors of 13195 are 5, 7, 13 and 29.
+; What is the largest prime factor of the number 600851475143?
 
 (defn prime? [n]
   (if (even? n)
@@ -14,10 +12,11 @@
     (zero? (mod a b))
     (prime? b)))
 
-(def start (bigint 600851475143))
+(defn largest_prime_factor [n]
+  (first
+    (filter #(prime_factor? n %)
+      (range (int (Math/sqrt n)) 1 -1))))
 
-(first
-  (filter #(prime_factor? start %)
-    (range (int (Math/sqrt start)) 1 -1)))
+(print
+  (largest_prime_factor 600851475143))
 ; 6857
-
