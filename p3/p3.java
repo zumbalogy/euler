4e60471f90281c4efe5dadb428c4637021bf84b4
zumbalogy <intrapostal@gmail.com>
2015-01-17 21:00:18 -0500

    p3 done in java

---
+++ p3/p3.java
@@ -0,0 +1,35 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143?
+
+public class p3 {
+  public static boolean prime(long input) {
+    double sq = Math.ceil(Math.sqrt(input));
+    if(input % 2 == 0) {
+      return false;
+    }
+    for(int i = 3; i <= sq; i += 2) {
+      if(input % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static Integer greatest_prime_factor(long input) {
+    int start = (int) Math.ceil(Math.sqrt(input));
+    if(start % 2 == 0) {
+      start += 1;
+    }
+    for(int i = start; true; i -= 2) {
+      if(input % i == 0 && prime(i)) {
+        return i;
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(greatest_prime_factor(600851475143L));
+  }
+}
+
+// 6857
