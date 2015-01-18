fbd3416fbceb07111a6197c1fe22bd757a1d0db5
zumbalogy <intrapostal@gmail.com>
2015-01-17 20:02:32 -0500

    still working on java

--- p1/p1.java
+++ p1/p1.java
@@ -1,9 +1,9 @@
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
 public class p1 {
-  public static Integer method() {
+  public static Integer sum_multiples_under(int limit) {
     int total = 0;
-    for(int i = 0; i < 1000; i++) {
+    for(int i = 0; i < limit; i++) {
       if(i % 3 == 0 || i % 5 == 0) {
         total += i;
       }
@@ -12,7 +12,7 @@ public class p1 {
   }
 
   public static void main(String[] args) {
-    System.out.println(method());
+    System.out.println(sum_multiples_under(1000));
   }
 }
 
