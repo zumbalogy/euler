6650453fbf0c739dee8443c33414d64b315f196c
zumbalogy <intrapostal@gmail.com>
2015-01-17 22:41:53 -0500

    clean

--- p4/p4.js
+++ p4/p4.js
@@ -1,32 +1,28 @@
-// look for the largest palindome number (9009) that
-// is the product of 2 3-diget numbers.
-
-// 906609
-// 913 * 993
+// Find the largest palindome number (9009) that is the product of 2 3-diget numbers.
 
 function is_pal(input){
-  var palness = true;
   var string = input.toString();
   var len = string.length;
 
   for (var i = 0; i < len; i++) {
-    var end = string[len - (i+1)];
-    if (string[i] != end) {
-      palness = false
+    if (string[i] != string[len - (1 + i)]) {
+      return false
     }
   }
-  return palness
+  return true
 }
 
 var output = 0;
 
-for (var x = 100; x < 1000; x++) {
-  for (var y = 100; y < 1000; y++) {
-    var foo = x * y;
-    if (foo > output && is_pal(foo)) {
-      output = foo
+for (var x = 999; x > 99; x--) {
+  for (var y = 999; y > 99; y--) {
+    var current = x * y;
+    if (current > output && is_pal(current)) {
+      output = current
     }
   }
-};
+}
 
 console.log(output)
+// 906609
+// 913 * 993
