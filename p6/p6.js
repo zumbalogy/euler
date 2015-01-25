03ed02c4e7d9a2889410b0c0523a87a568f3df64
zumbalogy <intrapostal@gmail.com>
2015-01-25 16:32:33 -0500

    clean of js p 6

--- p6/p6.js
+++ p6/p6.js
@@ -6,10 +6,12 @@
 // (1 + 2 + ... + 10)^2 = 55^2 = 3025
 // delta for sum of the squares of first ten natural numbers and square of the sum is 3025 − 385 = 2640
 
-// Find delta for sum of  squares of the first one hundred natural numbers and square of the sum
+// Find delta for sum of squares of the first one hundred natural numbers and square of the sum
 
-function e6(n){
-    return Math.pow(((n/2) * (n+1)), 2) - ( (n * (n+1) * ((2*n)+1)) / 6)
+function e6(n) {
+  var sumOfSqs = (n * (n + 1) * ((2 * n) + 1)) / 6
+  var sqOfSum = Math.pow(((n / 2) * (n + 1)), 2)
+  return sqOfSum - sumOfSqs
 }
 
 console.log(e6(100))
