bddb4300f51de01aabcd5ce620d6f03af296160d
zumbalogy <intrapostal@gmail.com>
2015-01-31 22:49:58 -0500

    clean

--- 102/p102.rb
+++ 102/p102.rb
@@ -5,7 +5,6 @@ Three distinct points are plotted at random on a Cartesian plane, for which -100
 Consider the following two triangles:
 
 A(-340,495), B(-153,-910), C(835,-947)
-
 X(-175,41), Y(-421,-714), Z(574,-645)
 
 It can be verified that triangle ABC contains the origin, whereas triangle XYZ does not.
@@ -16,51 +15,22 @@ NOTE: The first two examples in the file represent the triangles in the example 
 
 =end
 
-##############################################################
-
 triangles = File.readlines('./triangles.txt').map do |line|
-    tri = line.split(',').map(&:to_i)
-
-    # create a triangle array of three points
-    #   points are an array with [x, y] values.
-    [[tri[0],tri[1]],[tri[2],tri[3]],[tri[4],tri[5]]]
+  x1, y1, x2, y2, x3, y3 = line.split(',').map(&:to_i)
+  [[x1, y1], [x2, y2], [x3, y3]]
 end
 
-######################################################
-
 def same_side_as_origin?((x1, y1), (x2, y2), (x3, y3))
-    # point 1 & 2 make line, point 3 compared to origin
-
-    # slope (m) = (y2 -y1) / (x2 - x1)
-    m = (y2 - y1) / (x2 - x1).to_f
-
-    # y-intercept (b) = y1 - (m * x1)
-    b = y1 - (m * x1)
-
-    # y > (mx + b) AND (origin y) > (m(origin x) + b)
-    #   or both less than
-    #   origin x and origin y are zero, so are simplified
-    (y3 > (m * x3) + b && 0 > b) ||
-    (y3 < (m * x3) + b && 0 < b)
+  m = (y2 - y1) / (x2 - x1).to_f
+  b = y1 - (m * x1)
+  (y3 > (m * x3) + b && 0 > b) ||
+  (y3 < (m * x3) + b && 0 < b)
 end
 
 def tri_has_o?((p1, p2, p3))
-    # look at the 3 lines of a tringle, and compare
-    #   origin to the point of triangle not on givin line
-    #   iff true all times, point (origin) is contained
-    same_side_as_origin?(p1, p2, p3) &&
-    same_side_as_origin?(p1, p3, p2) &&
-    same_side_as_origin?(p2, p3, p1)
+  same_side_as_origin?(p1, p2, p3) &&
+  same_side_as_origin?(p1, p3, p2) &&
+  same_side_as_origin?(p2, p3, p1)
 end
 
-p triangles.count &method(:tri_has_o?)
-
-######################################################
-
-class Line
-    def initialize x1, y1, x2, y2
-        @slope = (x2 - x1) / (y2 - y1).to_f
-        @intercept = y1 - (@slope * x1)
-    end
-end
-
+p triangles.count(&method(:tri_has_o?))
