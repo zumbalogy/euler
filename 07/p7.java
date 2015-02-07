3432ab246cdf7644ae14b2d8e42d732a0e58b275
zumbalogy <intrapostal@gmail.com>
2015-02-07 14:33:56 -0500

    better 7 js and did 7 java

---
+++ 07/p7.java
@@ -0,0 +1,31 @@
+// What is the 10 001st prime number? (not including one)
+
+public class p7 {
+
+  static boolean oddPrime(int n) {
+    double limit = Math.sqrt(n);
+    for(int i = 3; i <= limit; i += 2) {
+      if(n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  static int nthPrime(int n) {
+    int current = 1;
+    while(n > 1) {
+      current += 2;
+      if(oddPrime(current)) {
+        n--;
+      }
+    }
+    return current;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(nthPrime(10001));
+  }
+}
+
+// 104743
--- 07/p7.js
+++ 07/p7.js
@@ -1,8 +1,8 @@
 // What is the 10 001st prime number? (not including one)
 
-function prime(input) {
+function oddPrime(input) {
   var limit = Math.sqrt(input)
-  for (var i = 3; i <= limit; i += 1) {
+  for (var i = 3; i <= limit; i += 2) {
     if (input % i == 0) {
       return false
     }
@@ -14,7 +14,7 @@ function nthPrime(n) {
   var i = 1
   while (n > 1) {
     i += 2
-    prime(i) && n--
+    oddPrime(i) && n--
   }
   return i
 }
