2ff8c81531ed49ca923754a254b478cb3187f398
zumbalogy <intrapostal@gmail.com>
2015-02-07 22:27:37 -0500

    clean

--- 09/p9.js
+++ 09/p9.js
@@ -6,16 +6,15 @@
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
 
-
 loop:
-for (var a = 1; a < 1000; a++){
-    for (var b = 1; b < 1000-a; b++){
-        var c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2))
-        if (a+b+c == 1000){
-            console.log(a*b*c)
-            break loop // or giv empty return. or this would be a function
-        }
+for (var a = 1; a < 1000; a++) {
+  for (var b = 1; b < 1000 - a; b++) {
+    var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+    if (a + b + c == 1000) {
+      console.log(a * b * c)
+      break loop // or give empty return. or this would be a function
     }
+  }
 }
 
 // 31875000
