961b99a82b90bb7bde41c57144d750fd149bc424
zumbalogy <intrapostal@gmail.com>
2015-02-23 00:37:44 -0500

    refactor

--- 03/p3.js
+++ 03/p3.js
@@ -1,21 +1,25 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143?
 
-function isPrime(input) {
-  if (input % 2 == 0)
-    return false
-  var sq = Math.sqrt(input)
-  for (var i = 3; i < sq; i += 2)
-    if (input % i == 0)
+function isPrime(n) {
+  for (var i = 3; i < Math.sqrt(n); i += 2) {
+    if (n % i == 0) {
       return false
+    }
+  }
   return true
 }
 
 function bigFactor(input) {
   var sq = Math.ceil(Math.sqrt(input))
-  for (var i = sq; i > 0; i--)
-    if (input % i == 0 && isPrime(i))
+  if (sq % 2 == 0) {
+    sq += 1
+  }
+  for (var i = sq; i > 0; i -= 2) {
+    if (input % i == 0 && isPrime(i)) {
       return i
+    }
+  }
 }
 
 console.log(bigFactor(600851475143))
