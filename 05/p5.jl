ff79dec16de725a483c378877684b80561f86768
sh <intrapostal@gmail.com>
2015-03-15 23:58:00 -0400

    clean

--- 05/p5.jl
+++ 05/p5.jl
@@ -2,9 +2,5 @@
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-gcd(a, b) = b == 0 ? a : gcd(b, a % b)
-
-lcm(a, b) = (a * b) / gcd(a, b)
-
 println(reduce(lcm, 11:20))
 # 232792560
