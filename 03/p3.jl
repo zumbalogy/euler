752bf3104981d7c043dd271e9ead251a9286fbfe
sh <intrapostal@gmail.com>
2015-03-15 22:22:51 -0400

    p3 julia better

--- 03/p3.jl
+++ 03/p3.jl
@@ -1,25 +1,16 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143?
 
-function is_prime(n)
-  n % 2 == 0 && return false
-  for i = 3:2:sqrt(n)
-    n % i == 0 && return false
-  end
-  true
-end
-
-
 function factor(n)
-  while !is_prime(n)
-    if n % 2 == 0
-      n /= 2
+  while n % 2 == 0
+    n /= 2
+  end
+  i = 3
+  while i < sqrt(n)
+    while n % i == 0
+      n /= i
     end
-    a = 3
-    while n % a > 0
-      a += 2
-    end
-    n /= a
+    i += 2
   end
   n
 end
