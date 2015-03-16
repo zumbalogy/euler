58a852358e9a3ba6ae00feff6f5d873dec6b5dbd
sh <intrapostal@gmail.com>
2015-03-15 22:44:05 -0400

    notes and clean

--- 03/p3.rb
+++ 03/p3.rb
@@ -1,8 +1,6 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143?
 
-# TODO: benchmark in the notes
-
 def factor(n)
   n /= 2 while n % 2 == 0
   i = 3
