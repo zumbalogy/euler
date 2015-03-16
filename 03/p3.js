38e4dabaddef79f5ca4c4754f3f8296bfdfadbf4
sh <intrapostal@gmail.com>
2015-03-15 22:28:53 -0400

    better p3 js

--- 03/p3.js
+++ 03/p3.js
@@ -1,19 +1,12 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143?
 
-function isPrime(n) {
-  for (var i = 3; i < Math.sqrt(n); i += 2) {
-    if (n % i == 0) { return false }
-  }
-  return true
-}
-
 function factor(n) {
-  while(!isPrime(n)) {
-    if (n % 2 == 0) { n /= 2 }
-    var a = 3
-    while(n % a != 0) { a += 2 }
-    n /= a
+  while(n % 2 == 0) { n /= 2 }
+  var i = 3
+  while(i < Math.sqrt(n)) {
+    while(n % i == 0) { n /= i }
+    i += 2
   }
   return n
 }
