066fc316e188848b90f610c799dc8ab6151fc5d1
sh <intrapostal@gmail.com>
2015-03-15 22:36:52 -0400

    better p3 java

--- 03/p3.java
+++ 03/p3.java
@@ -2,31 +2,19 @@
 // What is the largest prime factor of the number 600851475143?
 
 public class p3 {
-  static boolean prime(long input) {
-    double sq = Math.sqrt(input);
-    for(int i = 3; i <= sq; i += 2) {
-      if(input % i == 0) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   static long factor(long input) {
-    while(!prime(input)) {
-      if(input % 2 == 0) {
-        input /= 2;
+    while(input % 2 == 0) {
+      input /= 2;
+    }
+    int i = 3;
+    while(i < Math.sqrt(input)) {
+      while(input % i == 0) {
+        input /= i;
       }
-      for(double i = 3; true; i += 2) {
-        if((input / i) % 1 == 0) {
-          input /= i;
-          break;
-        }
-      }
+      i += 2;
     }
     return input;
   }
-
   public static void main(String[] args) {
     System.out.println(factor(600851475143L));
   }
