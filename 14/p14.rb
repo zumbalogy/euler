ef59b80d3b2f33294c0d3db765d87602c272c5ca
sh <intrapostal@gmail.com>
2015-04-11 15:45:58 -0400

    clean

--- 14/p14.rb
+++ 14/p14.rb
@@ -1,12 +1,11 @@
 # The following iterative sequence is defined for the set of positive integers:
 # n → n/2 (n is even)
 # n → 3n + 1 (n is odd)
-
+#
 # Using the rule above and starting with 13, we generate the following sequence:
 # 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-
-# It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
-
+# This sequence has 10 terms. It is not proven (Collatz Problem) but it is thought that all numbers go to 1.
+#
 # Which starting number, under one million, produces the longest chain?
 # NOTE: Once the chain starts the terms are allowed to go above one million.
 
@@ -15,21 +14,27 @@ def col_step(n)
   (3 * n) + 1
 end
 
-def col_count(n, total = 1)
-  return total unless n > 1
-  col_count(col_step(n), total + 1)
+def col_count(n)
+  count = 0
+  until n == 1
+    n = col_step(n)
+    count += 1
+  end
+  count
 end
 
-best = 0
-best_start = 0
-
-1_000_000.times do |i|
-  foo = col_count(i)
-  next unless foo > best
-  best = foo
-  best_start = i
+def best_count(limit)
+  best = 0
+  best_start = 0
+  (1..limit).each do |i|
+    count = col_count(i)
+    next unless count > best
+    best = count
+    best_start = i
+  end
+  best_start
 end
 
-puts best_start
-# 837,799
+puts best_count(1000000)
+# 837799
 # count of 525
