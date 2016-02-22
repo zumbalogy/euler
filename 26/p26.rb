f50cf4ddbab0e503888579590f4592bf0f1788bd
zumbalogy <intrapostal@gmail.com>
2016-02-22 00:17:23 -0500

    some initial work on p26

--- 26/p26.rb
+++ 26/p26.rb
@@ -15,3 +15,28 @@
 # It can be seen that 1/7 has a 6-digit recurring cycle.
 
 # Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+
+def loop_len(x)
+  i = 1
+  tail = x[-i]
+  len = x.length
+  while x[0..len - i - 1].index(tail)
+    i += 1
+    tail = x[(len - i)..-1]
+  end
+  tail.length - 1
+end
+
+best = 0
+
+(1..1001).each do |t|
+  current = loop_len((1.0 / t).to_s)
+  if current > best
+    best = current
+  end
+end
+
+# 0.09009009009009009
+
+# this is subject to being fooled by not realizing its the end of the loop. it thinks this has a loop length of 9
+# to fix this, i could check in the loop to make sure the number of non-overlapping matches does not decrease
