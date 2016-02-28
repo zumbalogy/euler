86ef8e25d8da694c4c8304f995cf89fc4ab5d01e
zumbalogy <intrapostal@gmail.com>
2016-02-28 12:13:10 -0500

    added specs from https://gist.github.com/mjansen401/3785573

--- other/gilded-rose-clojure/project.clj
+++ other/gilded-rose-clojure/project.clj
@@ -1,8 +1,6 @@
-(defproject gilded-rose "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject gilded-rose "0.1.0"
+  :description "Guilded Rose Kata"
+  :url "https://github.com/zumbalogy/euler/tree/master/other/gilded-rose-clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[speclj "2.2.0"]]}}
   :plugins [[speclj "2.2.0"]]
