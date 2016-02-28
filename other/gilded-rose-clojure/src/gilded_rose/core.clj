a27e27a362bb5f47b08c9f0827e315ab29529b7c
zumbalogy <intrapostal@gmail.com>
2016-02-28 12:22:33 -0500

    clean

--- other/gilded-rose-clojure/src/gilded_rose/core.clj
+++ other/gilded-rose-clojure/src/gilded_rose/core.clj
@@ -32,13 +32,9 @@
   {:name item-name, :sell-in sell-in, :quality quality})
 
 (defn update-current-inventory[]
-  (let [inventory
-    [
-      (item "+5 Dexterity Vest" 10 20)
-      (item "Aged Brie" 2 0)
-      (item "Elixir of the Mongoose" 5 7)
-      (item "Sulfuras, Hand Of Ragnaros" 0 80)
-      (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
-    ]]
-    (update-quality inventory)
-    ))
+  (let [inventory [(item "+5 Dexterity Vest" 10 20)
+                   (item "Aged Brie" 2 0)
+                   (item "Elixir of the Mongoose" 5 7)
+                   (item "Sulfuras, Hand Of Ragnaros" 0 80)
+                   (item "Backstage passes to a TAFKAL80ETC concert" 15 20)]]
+    (update-quality inventory)))
