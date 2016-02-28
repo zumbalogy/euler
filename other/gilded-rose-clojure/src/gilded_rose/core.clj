463a0a9a729e16d4589cc98752f2ded74f42feeb
zumbalogy <intrapostal@gmail.com>
2016-02-28 12:51:16 -0500

    chopping away a little bit

--- other/gilded-rose-clojure/src/gilded_rose/core.clj
+++ other/gilded-rose-clojure/src/gilded_rose/core.clj
@@ -1,31 +1,39 @@
 (ns gilded-rose.core)
 
+(def concert "Backstage passes to a TAFKAL80ETC concert")
+
 (defn mapper-one [item]
   (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
     (merge item {:sell-in (dec (:sell-in item))})
     item))
 
+(defn change-quality [item change]
+  (merge item {:quality (+ change (:quality item))}))
+
 (defn mapper-two [item]
-  (cond
-    (and (neg? (:sell-in item)) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
-      (merge item {:quality 0})
-    (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-      (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
-        (merge item {:quality (+ 2 (:quality item))})
-        (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
-          (merge item {:quality (+ 3 (:quality item))})
-          (if (< (:quality item) 50)
-            (merge item {:quality (inc (:quality item))})
-            item)))
-    (neg? (:sell-in item))
-      (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
+  (let [name (:name item)
+        quality (:quality item)
+        sell-in (:sell-in item)]
+    (cond
+      (and (neg? sell-in) (= concert name))
         (merge item {:quality 0})
-        (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-          (merge item {:quality (- (:quality item) 2)})
-          item))
-    (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-      (merge item {:quality (dec (:quality item))})
-    :else item))
+      (or (= name "Aged Brie") (= name concert))
+        (if (and (= name concert) (< 4 sell-in 10))
+          (change-quality item 2)
+          (if (and (= name concert) (<= 0 sell-in 4))
+            (change-quality item 3)
+            (if (< quality 50)
+              (change-quality item 1)
+              item)))
+      (neg? sell-in)
+        (if (= concert name)
+          (merge item {:quality 0})
+          (if (or (= "+5 Dexterity Vest" name) (= "Elixir of the Mongoose" name))
+            (merge item {:quality (- quality 2)})
+            item))
+      (or (= "+5 Dexterity Vest" name) (= "Elixir of the Mongoose" name))
+        (merge item {:quality (dec quality)})
+      :else item)))
 
 (defn update-quality [items]
   (map mapper-two (map mapper-one items)))
@@ -38,5 +46,5 @@
                    (item "Aged Brie" 2 0)
                    (item "Elixir of the Mongoose" 5 7)
                    (item "Sulfuras, Hand Of Ragnaros" 0 80)
-                   (item "Backstage passes to a TAFKAL80ETC concert" 15 20)]]
+                   (item concert 15 20)]]
     (update-quality inventory)))
