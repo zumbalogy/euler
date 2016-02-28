3de481a929b0ee490feeb301480682fedb902492
zumbalogy <intrapostal@gmail.com>
2016-02-28 13:29:26 -0500

    got concert date stuff sorted out with new dec-ing date after the fact

--- other/gilded-rose-clojure/spec/gilded_rose/core_spec.clj
+++ other/gilded-rose-clojure/spec/gilded_rose/core_spec.clj
@@ -32,52 +32,51 @@
   (:quality (item-by-name item-name updated-items)))
 
 (describe "gilded rose"
-  (describe "acceptance tests"
-    (describe "sell_in values"
-      (it "decreases by 1 after one update for non-legendary items"
-        (let [[updated-items] [(update-quality-n-times inventory 1)]]
-          (should= 9 (updated-sell-in vest updated-items))
-          (should= 1 (updated-sell-in brie updated-items))
-          (should= 4 (updated-sell-in elixir updated-items))
-          (should= 14 (updated-sell-in passes updated-items))))
+  (describe "sell_in values"
+    (it "decreases by 1 after one update for non-legendary items"
+      (let [[updated-items] [(update-quality-n-times inventory 1)]]
+        (should= 9 (updated-sell-in vest updated-items))
+        (should= 1 (updated-sell-in brie updated-items))
+        (should= 4 (updated-sell-in elixir updated-items))
+        (should= 14 (updated-sell-in passes updated-items))))
 
-      (it "decreases by 2 after two updates for non-legendary items"
-        (let [[updated-items] [(update-quality (update-quality-n-times inventory 1))]]
-          (should= 8 (updated-sell-in vest updated-items))
-          (should= 0 (updated-sell-in brie updated-items))
-          (should= 3 (updated-sell-in elixir updated-items))
-          (should= 13 (updated-sell-in passes updated-items))))
+    (it "decreases by 2 after two updates for non-legendary items"
+      (let [[updated-items] [(update-quality (update-quality-n-times inventory 1))]]
+        (should= 8 (updated-sell-in vest updated-items))
+        (should= 0 (updated-sell-in brie updated-items))
+        (should= 3 (updated-sell-in elixir updated-items))
+        (should= 13 (updated-sell-in passes updated-items))))
 
-      (it "stays constant for legendary items"
-        (let [[items-updated-once] [(update-quality-n-times inventory 1)]]
-          (should= 0 (updated-sell-in sulfuras items-updated-once))
-          (should= 0 (updated-sell-in sulfuras (update-quality items-updated-once))))))
+    (it "stays constant for legendary items"
+      (let [[items-updated-once] [(update-quality-n-times inventory 1)]]
+        (should= 0 (updated-sell-in sulfuras items-updated-once))
+        (should= 0 (updated-sell-in sulfuras (update-quality items-updated-once))))))
 
-    (describe "quality values"
-      (describe "normal items"
-        (it "decreases the quality by 1"
-          (should= 19 (updated-quality vest (update-quality-n-times inventory 1)))
-          (should= 6 (updated-quality elixir (update-quality-n-times inventory 1))))
-        (it "decreases the quality by 2 when item expired"
-          (should= 8 (updated-quality vest (update-quality-n-times inventory 11)))
-          (should= 0 (updated-quality elixir (update-quality-n-times inventory 6)))))
+  (describe "quality values"
+    (describe "normal items"
+      (it "decreases the quality by 1"
+        (should= 19 (updated-quality vest (update-quality-n-times inventory 1)))
+        (should= 6 (updated-quality elixir (update-quality-n-times inventory 1))))
+      (it "decreases the quality by 2 when item expired"
+        (should= 8 (updated-quality vest (update-quality-n-times inventory 11)))
+        (should= 0 (updated-quality elixir (update-quality-n-times inventory 6)))))
 
-      (describe "aged brie"
-        (it "increases in quality as it ages"
-          (should= 1 (updated-quality brie (update-quality-n-times inventory 1))))
-        (it "never gets above 50 quality"
-          (should= 50 (updated-quality brie (update-quality-n-times inventory 55)))))
+    (describe "aged brie"
+      (it "increases in quality as it ages"
+        (should= 1 (updated-quality brie (update-quality-n-times inventory 1))))
+      (it "never gets above 50 quality"
+        (should= 50 (updated-quality brie (update-quality-n-times inventory 55)))))
 
-      (describe "sulfuras"
-        (it "never decreases in quality"
-          (should= 80 (updated-quality sulfuras (update-quality-n-times inventory 10)))))
+    (describe "sulfuras"
+      (it "never decreases in quality"
+        (should= 80 (updated-quality sulfuras (update-quality-n-times inventory 10)))))
 
-      (describe "passes"
-        (it "increases in quality by 1 with more than 10 days to sell"
-          (should= 25 (updated-quality passes (update-quality-n-times inventory 5))))
-        (it "increases in quality by 2 with more than 5 days and less than 10 to sell"
-          (should= 35 (updated-quality passes (update-quality-n-times inventory 10))))
-        (it "increases in quality by 3 with more than 0 days and less than 5 to sell"
-          (should= 50 (updated-quality passes (update-quality-n-times inventory 15))))
-        (it "drops to 0 when expired"
-          (should= 0 (updated-quality passes (update-quality-n-times inventory 16))))))))
+    (describe "passes"
+      (it "increases in quality by 1 with more than 10 days to sell"
+        (should= 25 (updated-quality passes (update-quality-n-times inventory 5))))
+      (it "increases in quality by 2 with more than 5 days and less than 10 to sell"
+        (should= 35 (updated-quality passes (update-quality-n-times inventory 10))))
+      (it "increases in quality by 3 with more than 0 days and less than 5 to sell"
+        (should= 50 (updated-quality passes (update-quality-n-times inventory 15))))
+      (it "drops to 0 when expired"
+        (should= 0 (updated-quality passes (update-quality-n-times inventory 16)))))))
--- other/gilded-rose-clojure/src/gilded_rose/core.clj
+++ other/gilded-rose-clojure/src/gilded_rose/core.clj
@@ -1,38 +1,34 @@
 (ns gilded-rose.core)
 
-(def concert "Backstage passes to a TAFKAL80ETC concert")
-
 (defn change-quality [item change]
-  (merge item {:quality (+ change (:quality item))}))
+  (merge item {:quality (+ change (:quality item))
+               :sell-in (dec (:sell-in item))}))
 
 (defn handle-concert [item]
   (let [quality (:quality item)
         sell-in (:sell-in item)]
     (cond
-      (neg? sell-in)
+      (> 1 sell-in)
         (merge item {:quality 0})
-      (<= 0 sell-in 4)
+      (<= 0 sell-in 5)
         (change-quality item 3)
-      (< 4 sell-in 10)
+      (<= 6 sell-in 10)
         (change-quality item 2)
       (< quality 50)
         (change-quality item 1)
       :else item)))
 
 (defn mapper [item]
-  (let [item (if (= "Sulfuras, Hand of Ragnaros" (:name item))
-               item
-               (merge item {:sell-in (dec (:sell-in item))}))
-        name (:name item)
+  (let [name (:name item)
         quality (:quality item)
         sell-in (:sell-in item)]
     (cond
       (and (= name "Aged Brie") (< quality 50))
         (change-quality item 1)
-      (= name concert)
+      (= name "Backstage passes to a TAFKAL80ETC concert")
         (handle-concert item)
       (or (= "+5 Dexterity Vest" name) (= "Elixir of the Mongoose" name))
-        (if (neg? sell-in)
+        (if (> 1 sell-in)
           (change-quality item -2)
           (change-quality item -1))
       :else item)))
@@ -48,5 +44,5 @@
                    (item "Aged Brie" 2 0)
                    (item "Elixir of the Mongoose" 5 7)
                    (item "Sulfuras, Hand Of Ragnaros" 0 80)
-                   (item concert 15 20)]]
+                   (item "Backstage passes to a TAFKAL80ETC concert" 15 20)]]
     (update-quality inventory)))
