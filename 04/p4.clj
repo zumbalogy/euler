26d255463d35956e14ea39270c6a0c9e3bd7a401
zumbalogy <intrapostal@gmail.com>
2016-03-13 18:20:09 -0400

    clean and naming

--- 04/p4.clj
+++ 04/p4.clj
@@ -1,15 +1,19 @@
 ; Find the largest palindome number (9009) that
 ; is the product of 2 3-digit numbers.
 
-(require '[clojure.string :as string])
-
 (defn to-pal [n]
-  (Integer. (str n (string/reverse (str n)))))
+  (Integer. (str n (clojure.string/reverse (str n)))))
 
-(defn pal-prod [n]
-  (some #(and (== 0 (mod n %)) (< (/ n %) 1000)) (range 100 1000)))
+(defn valid-prod [a b]
+  (and (== 0 (mod a b))
+       (> 1000 (/ a b))))
 
-(println (to-pal
-  (first (filter #(pal-prod (to-pal %)) (reverse (range 1 1000))))))
+(defn has-prod [n]s
+
+(println
+  (->> (reverse (range 1 1000))
+       (filter #(has-prod (to-pal %)))
+       first
+       to-pal))
 ; 906609
 ; from 913 993
--- 04/p4.rb
+++ 04/p4.rb
@@ -5,14 +5,14 @@ def to_pal(n)
   (n.to_s + n.to_s.reverse).to_i
 end
 
-def pal_product?(n)
+def best_product?(n)
   (100..1000).any? do |i|
     (n % i == 0) && (n / i) < 1000
   end
 end
 
 range = (1..1000).reverse_each
-match = range.find { |i| pal_product?(to_pal(i)) }
+match = range.find { |i| best_product?(to_pal(i)) }
 large_pal = to_pal(match)
 
 puts large_pal
