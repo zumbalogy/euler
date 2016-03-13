f52e8d6d10814338a247edb126406047bfbe1245
zumbalogy <intrapostal@gmail.com>
2016-03-13 17:06:29 -0400

    better usage of main logical function in p4 clj

--- 04/p4.clj
+++ 04/p4.clj
@@ -1,24 +1,19 @@
 ; look for the largest palindome number (9009) that
 ; is the product of 2 3-digit numbers.
 
-(use '[clojure.string :only (join split)])
+(use '[clojure.string :only [join split]])
 
-(defn is_pal [input]
+(defn is-pal [input]
   (= (str input) (join (reverse (str input)))))
 
-(defn loop_check [input]
+(defn loop-check [input]
   (loop [x 999]
-    (if (< 99 x)
-      (if (is_pal (* x input))
+    (when (< 99 x)
+      (if (is-pal (* x input))
         (* x input)
         (recur (dec x))))))
 
-(loop [x 999 best 0]
-  (if (< 99 x)
-    (recur
-      (dec x)
-      (max best (or (loop_check x) 0)))
-    (print best)))
-
+(println
+  (apply max (remove nil? (map loop-check (range 99 1000)))))
 ; 906609
 ; from 913 993
