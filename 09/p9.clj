94229dae8ff69031247a674201a572d7117b3d32
zumbalogy <intrapostal@gmail.com>
2016-03-13 22:54:45 -0400

    p9 clojure

--- 09/p9.clj
+++ 09/p9.clj
@@ -5,54 +5,18 @@
 ; There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 ; Find the product abc.
 
-(defn hit [a b]
+(defn first-from [func x]
+  (first (remove nil? (map func x))))
+
+(defn trip [a b]
   (let [c (Math/sqrt (+ (* a a) (* b b)))]
-    (== 1000 (+ a b c))))
+    (when (== 1000 (+ a b c))
+      [a b c])))
 
-(defn loopcheck [start]
-  (loop [x start]
-    (when (< x 500)
-      (if (hit start x)
-        [start x]
-        (recur (+ 1 x))))))
+(defn my-check [a]
+  (first-from #(trip a %) (range a 500)))
 
-(print (loop [a 0]
-  (if (loopcheck a)
-    (let [b (last (loopcheck a))]
-      [(loopcheck a) (Math/sqrt (+ (* a a) (* b b)))])
-    (recur (+ 1 a)))))
-
-; (loop [a 0]
-;   (loop [b a]
-;     (let [c (+ (* a a) (* b b))])
-;     ))
-
-; (loop [x 10]
-;   (when (> x 1)
-;     (println x)
-;     (recur (- x 2))))
-
-;# 200
-;# 375
-;# 425
-;# ==
-;# 31_875_000
-
-; a + b + c = 1000
-
-; a + b + sqrt(a**2 + b**2) == 1000
-
-; sqrt(a**2 + b**2) == 1000 - a - b
-
-; a**2 + b**2 == (1000 - a - b) * (1000 - a - b)
-
-; a**2 + b**2 == (1000 - a - b) ** 2
-
-; ;;;;;;;;;;;;;;
-
-; b**2 == ((1000 - a - b) ** 2) - a**2
-
-; b == Math.sqrt(((1000 - a - b) ** 2) - a**2)
-
-; well, this seems a dead end
-
+(println
+  (reduce * (first-from my-check (range 1 500))))
+; # 31875000
+; # 200 375 425
