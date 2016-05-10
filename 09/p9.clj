182b9622a6560203c32d4db51bea50e9bf75178f
zumbalogy <intrapostal@gmail.com>
2016-05-10 15:36:56 -0400

    clean notes

--- 09/p9.clj
+++ 09/p9.clj
@@ -1,9 +1,9 @@
-; A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+; A Pythagorean triplet is a set of three natural numbers, a < b < c, for which
 
 ; a^2 + b^2 = c^2
-; For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
-; There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-; Find the product abc.
+; For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2
+; There is only one Pythagorean triplet for which a + b + c = 1000
+; Find the product abc
 
 (defn trip [a b]
   (let [c (Math/sqrt (+ (* a a) (* b b)))]
@@ -15,5 +15,5 @@
 
 (println
   (reduce * (some my-check (range))))
-; # 31875000
-; # 200 375 425
+; 31875000
+; 200 375 425
