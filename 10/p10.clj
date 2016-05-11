1cff973a2a7fd84e08717ef7c5a0d790f66ef2ab
zumbalogy <intrapostal@gmail.com>
2016-05-10 21:16:24 -0400

    clean

--- 10/p10.clj
+++ 10/p10.clj
@@ -2,9 +2,9 @@
 
 ; Find the sum of all the primes below two million.
 
-(defn is_prime [input]
-  (not-any? #(== 0 (mod input %)) (range 3 (inc (Math/sqrt input)) 2)))
+(defn prime? [n]
+  (not-any? #(== 0 (mod n %)) (range 3 (inc (Math/sqrt n)) 2)))
 
 (println
-  (reduce + 2 (filter is_prime (range 3 2000000 2))))
+  (reduce + 2 (filter prime? (range 3 2000000 2))))
 ; 142_913_828_922
