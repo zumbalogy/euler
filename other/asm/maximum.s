30e028e1080636d253e245b8da85b48805c2e2a4
zumbalogy <intrapostal@gmail.com>
2016-05-19 12:45:12 -0400

    clean and small optimazation

--- other/asm/maximum.s
+++ other/asm/maximum.s
@@ -16,7 +16,7 @@
 
 .section .data
 
-data_items:         #These are the data items
+data_items:         # These are the data items
  .long 3,67,34,222,45,75,54,34,44,33,22,11,66,0
 
  .section .text
@@ -26,14 +26,12 @@ data_items:         #These are the data items
 _start:
  movl $0, %edi     # move 0 into the largest register
  movl data_items(,%edi,4), %eax # load the first byte of data
- movl %eax, %ebx   # since this is the first item, %eax is
-                   # the biggest
 
 start_loop:        # start loop
  cmpl $0, %eax     # check to see if we have hit the end
  je loop_exit
- incl %edi         # load next value
  movl data_items(,%edi,4), %eax
+ incl %edi         # load next value
  cmpl %ebx, %eax   # compare values
  jle start_loop    # jump to beginning of loop if new
                    # one is not bigger
