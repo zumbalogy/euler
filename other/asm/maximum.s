6ce7b2ab65ab9b1d9dc0ced011818474d240f57f
zumbalogy <intrapostal@gmail.com>
2016-05-22 19:43:08 -0400

    clean

--- other/asm/maximum.s
+++ other/asm/maximum.s
@@ -16,7 +16,7 @@
 
 .section .data
 
-data_items:         # These are the data items
+data_items:
  .long 3,67,34,222,45,75,54,34,44,33,22,11,66,0
 
  .section .text
@@ -24,20 +24,19 @@ data_items:         # These are the data items
  .globl _start
 
 _start:
- movl $0, %edi     # move 0 into the largest register
- movl data_items(,%edi,4), %eax # load the first byte of data
+ movl $0, %edi     # move 0 into the index register
 
-start_loop:        # start loop
+start_loop:
+ movl data_items(,%edi,4), %eax
+ incl %edi         # increment index
  cmpl $0, %eax     # check to see if we have hit the end
  je loop_exit
- movl data_items(,%edi,4), %eax
- incl %edi         # load next value
  cmpl %ebx, %eax   # compare values
  jle start_loop    # jump to beginning of loop if new
                    # one is not bigger
 
  movl %eax, %ebx   # move the value as the largest
- jmp start_loop    # jump to loop beginning
+ jmp start_loop
 
 loop_exit:
  # %ebx is the status code for the exit system call
