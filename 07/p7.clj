9d31c1f9108c478bf1837281bd84741056253356
zumbalogy <intrapostal@gmail.com>
2016-08-27 18:02:11 -0400

    polish up p7 clj

--- 07/p7.clj
+++ 07/p7.clj
@@ -1,39 +1,12 @@
-;; What is the 10 001st prime number? (not including one)
+;; What is the 10 001st prime number?
 
-(defn is_prime [input]
-  (if (even? input)
+(defn prime? [n]
+  (if (even? n)
     false
-    (= [1]
-       (for [x (range 1 (inc (Math/sqrt input)) 2)
-          :when (== (mod input x) 0)] x))))
+    (not-any? #(zero? (mod n %))
+      (range 3 (int (inc (Math/sqrt n))) 2))))
 ; gives true for 1, false for 2
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(last (take 10001 (filter is_prime (range))))
-; 104743
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(def prime_count (atom 0))
-(def check_num (atom 0))
-
-(while (> 10001 @prime_count)
-  (do
-    (swap! check_num inc)
-    (if (is_prime @check_num)
-      (swap! prime_count inc))))
-
-; (print @check_num)
-; 104743
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(loop [prime_count 0 current 1]
-  (if (is_prime current)
-    (if (= prime_count 10000)
-      (print current)
-      (recur (inc prime_count) (+ 2 current)))
-    (recur prime_count (inc current))))
-
+(println
+  (nth (filter prime? (range)) 10000))
 ; 104743
