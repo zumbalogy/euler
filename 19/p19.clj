0907de1c1ae1ea55a3b357b1821ce0e7cade3702
zumbalogy <intrapostal@gmail.com>
2016-08-28 23:12:06 -0400

    moved things around

--- 19/p19.clj
+++ 19/p19.clj
@@ -9,11 +9,11 @@
 ; A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 ; How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
-(def start {:date 1 :day 0 :month 0 :year 1900})
-
 (defn tick-date [date month year]
-  (let [month-day-count [31 (if (= 0 (mod year 4)) 29 28) 31 30 31 30 31 31 30 31 30 31]]
-    (mod (inc date) (get month-day-count month))))
+  (let [feb (if (= 0 (mod year 4)) 29 28)
+        day-counts [31 feb 31 30 31 30 31 31 30 31 30 31]
+        current (get day-counts month)]
+    (mod (inc date) current)))
 
 (defn tick [{date :date day :day month :month year :year}]
   (let [new-date (tick-date date month year)
@@ -22,8 +22,11 @@
         new-year (if (and (= 0 new-date) (= 0 new-month)) (inc year) year)]
     {:date new-date :day new-day :month new-month :year new-year}))
 
+(def start {:date 1 :day 0 :month 0 :year 1900})
+
 (def century
-  (filter #(< 1900 (:year %)) (take-while #(> 2001 (:year %)) (iterate tick start))))
+  (filter #(< 1900 (:year %))
+    (take-while #(> 2001 (:year %)) (iterate tick start))))
 
 (println
   (count (filter #(and (= 0 (:date %)) (= 6 (:day %))) century)))
