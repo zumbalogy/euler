18392827734bb24a8ca61311acb40c3f5a3e3f97
zumbalogy <intrapostal@gmail.com>
2016-09-03 21:59:30 -0400

    refactor p3 haskell

--- 03/p3.hs
+++ 03/p3.hs
@@ -1,12 +1,11 @@
 -- The prime factors of 13195 are 5, 7, 13 and 29.
 -- What is the largest prime factor of the number 600851475143?
 
-factor 1 = 1
-factor x = let z = [div x y | y <- [2..x], (div x y) * y == x]
-           in if (null z) then 1 else (head z)
+lpf a b = if a < b * b
+          then a
+          else if (div a b) * b == a
+               then lpf (div a b) b
+               else lpf a (b + 1)
 
-factors n = takeWhile (\x -> x /= 1) [x | x <- (iterate factor n)]
-topFactor n = last (factors n)
-
-main = putStrLn (show (topFactor 600851475143))
+main = putStrLn (show (lpf 600851475143 2))
 -- 6857
