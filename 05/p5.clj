09770bfb73f085675035ccdb46a8d18181f4de60
zumbalogy <intrapostal@gmail.com>
2016-09-03 22:49:57 -0400

    p5 haskell and rem over mod for clj 5

--- 05/p5.clj
+++ 05/p5.clj
@@ -1,11 +1,11 @@
-; 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+; 2520 is the smallest number that can be divided by the numbers from 1 to 10 without any remainder.
 
-; What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+; What is the smallest positive number that is evenly divisible by the numbers from 1 to 20?
 
 (defn gdc [a b]
   (if (= b 0)
     a
-    (gdc b (mod a b))))
+    (gdc b (rem a b))))
 
 (defn lcm [a b]
   (/ (* a b) (gdc a b)))
---
+++ 05/p5.hs
@@ -0,0 +1,6 @@
+-- 2520 is the smallest number that can be divided by the numbers from 1 to 10 without any remainder.
+
+-- What is the smallest positive number that is evenly divisible by the numbers from 1 to 20?
+
+main = putStrLn (show (foldl lcm 1 [11..20]))
+-- 232792560
