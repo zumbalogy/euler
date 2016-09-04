7ee88db60c45d430e9dcf15d3d61302c4cfa8c24
zumbalogy <intrapostal@gmail.com>
2016-09-03 21:40:44 -0400

    p3 haskell and clean 1

--- 01/p1.hs
+++ 01/p1.hs
@@ -2,7 +2,7 @@
 -- The sum of these multiples is 23.
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
-total = sum [x | x <- [1..999], or [(mod x 5 == 0), (mod x 3 == 0)]]
+total = sum [x | x <- [1..999], (mod x 5 == 0) || (mod x 3 == 0)]
 
 main = putStrLn (show total)
 -- 233168
