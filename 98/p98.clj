2c2b271c982fd0f5b269cc479b008ce6097c974a
zumbalogy <intrapostal@gmail.com>
2017-03-07 11:29:09 +0700

    clean

--- 98/p98.clj
+++ 98/p98.clj
@@ -15,14 +15,6 @@
 ; It seems like number subsitutions have to be 0-9 for a letter, and no letters allowed to map to same value
 ; so all words with over 10 different letters are disqualified.
 
-(defn make-pairs [n]
-  (vec (set
-    (filter #(apply not= %)
-      (for [a n b n] (sort [a b]))))))
-
-(defn anagram->code [a b]
-  (map #(.indexOf (str b) (str %)) (str a)))
-
 ; https://burningmath.blogspot.com/2013/09/how-to-check-if-number-is-perfect-square.html
 ; https://stackoverflow.com/questions/295579/fastest-way-to-determine-if-an-integers-square-root-is-an-integer
 (defn square? [n]
@@ -31,29 +23,29 @@
     (when (or (= h 0) (= h 1) (= h 4) (= h 9))
       (= n (* t t)))))
 
+(defn make-pairs [n]
+  (vec (set
+    (filter #(apply not= %)
+      (for [a n b n] (sort [a b]))))))
+
+(defn sym-freqs [n]
+  (sort (map last (frequencies (str n)))))
+
+(defn anagram->code [a b]
+  (map #(.indexOf (str b) (str %)) (str a)))
+
 (defn transform [n code]
   (let [digits (map #(Character/digit % 10) (str n))
         out (map #(nth digits %) code)]
     (when (and (every? identity out)
                (not= (apply str out) (str n))
                (pos? (first out)))
-      (read-string (apply str out))))) ; might be faster to try/catch or maybe dont use read-string
-
-(defn sym-freqs [n]
-  (sort (map last (frequencies (str n)))))
-
-(defn digit-count [n]
-  (inc (int (Math/log10 n))))
+      (read-string (apply str out)))))
 
 (defn get-sq-anagrams [[code words]]
-  (->>
-      ;  (range (int (Math/sqrt (Math/pow 10 (dec (count code))))) (Math/sqrt (Math/pow 10 (count code))))
-      ;  (range 1 (Math/sqrt (Math/pow 10 (count code))))
-      (range (Math/sqrt (Math/pow 10 (count code))))
-      (drop (int (Math/sqrt (Math/pow 10 (dec (count code))))))
+  (->> (Math/sqrt (Math/pow 10 (count code)))
+       (range (int (Math/sqrt (Math/pow 10 (dec (count code))))))
        (map #(* % %))
-      ;  (drop-while #(> (count code) (digit-count %)))
-      ;  (take-while #(= (count code) (digit-count %)))
        (filter #(= (sym-freqs (first words)) (sym-freqs %)))
        (map #(list (transform % code) %))
        (filter first)
