1bb9d2b9c99bb166d3bdbc00b7c8f39e9df634d1
zumbalogy <intrapostal@gmail.com>
2017-03-13 21:14:42 +0700

    cleaner towers of hanoi, and notes/misc with it

--- other/hanoi.clj
+++ other/hanoi.clj
@@ -1,15 +1,28 @@
 (defn towers
   ([n]
-    (towers n 1 2 3 []))
-  ([n a b c out]
+    (towers n [] 1 2 3))
+  ([n out a b c]
     (if (= 1 n)
-      (conj out [a b])
+      [[a b]]
       (concat
-        (towers (dec n) a c b out)
-        (concat [[a b]]
-          (towers (dec n) c b a out))))))
+        (towers (dec n) out a c b)
+        [[a b]]
+        (towers (dec n) out c b a)))))
 
+(println (towers 1))
+(println (towers 2))
+(println (towers 3))
+
+(time (towers 3))
+(time (towers 21))
 
 (assert (= (towers 1) [[1 2]]))
 (assert (= (towers 2) [[1 3] [1 2] [3 2]]))
 (assert (= (towers 3) [[1 2] [1 3] [2 3] [1 2] [3 1] [3 2] [1 2]]))
+
+
+;                                                             1.2
+;                             1.3                             1.2                             3.2
+;             1.2             1.3             2.3             1.2             3.1             3.2             1.2
+;     1.3     1.2     3.2     1.3     2.1     2.3     1.3     1.2     3.2     3.1     2.1     3.2     1.3     1.2     3.2
+; 1.2 1.3 2.3 1.2 3.1 3.2 1.2 1.3 2.3 2.1 3.1 2.3 1.2 1.3 2.3 1.2 3.1 3.2 1.2 3.1 2.3 2.1 3.1 3.2 1.2 1.3 2.3 1.2 3.1 3.2 1.2
