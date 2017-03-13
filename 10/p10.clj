673a2676a6267a46d7886721c043f9fb0c589aa9
zumbalogy <intrapostal@gmail.com>
2017-03-13 14:29:06 +0700

    using sieve for primes in p10 clojure, for over 10x speedup

--- 10/p10.clj
+++ 10/p10.clj
@@ -2,11 +2,15 @@
 
 ; Find the sum of all the primes below two million.
 
-; TODO: slow, use a sieve or something. 
-
-(defn prime? [n]
-  (not-any? #(zero? (rem n %)) (range 3 (inc (Math/sqrt n)) 2)))
+(defn primes-until [n]
+  (let [bs (boolean-array n true)]
+    (doseq [i (range 2 (Math/sqrt n))]
+      (when (nth bs i)
+        (doseq [x (range (* i i) n i)]
+          (aset-boolean bs x false))))
+      (drop 2 (keep-indexed #(when %2 %1) bs))))
 
 (println
-  (reduce + 2 (filter prime? (range 3 2000000 2))))
-; 142_913_828_922
+  (reduce + (primes-until 2000000)))
+; 142913828922
+; Note: half the time (about 500ms for me) is spent doing the keep-indexed
