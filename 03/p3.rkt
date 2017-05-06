fbec82981eef3461c3307a4e01c3fb6941d56caa
zumbalogy <intrapostal@gmail.com>
2017-05-06 18:40:18 +0900

    clean p3 rkt

--- 03/p3.rkt
+++ 03/p3.rkt
@@ -3,13 +3,15 @@
 ; The prime factors of 13195 are 5, 7, 13 and 29.
 ; What is the largest prime factor of the number 600851475143?
 
+(define (lpf-loop i input)
+  (if (< input (* i i))
+    input
+    (if (= 0 (remainder input i))
+      (lpf-loop i (/ input i))
+      (lpf-loop (+ 1 i) input))))
+
 (define (lpf input)
-  (let loop ([i 2] [out input])
-    (if (< out (* i i))
-      out
-      (if (= 0 (remainder out i))
-        (loop i (/ out i))
-        (loop (+ 1 i) out)))))
+  (lpf-loop 2 input))
 
 (println (lpf 600851475143))
 ; 6857
