ad32cd2a5410a03832707521f22cd6df90fca7bf
zumbalogy <intrapostal@gmail.com>
2017-06-10 16:59:35 +0900

    converting the subarrays to ranked lists to better memoize does not seem to help

--- 24/p24.clj
+++ 24/p24.clj
@@ -8,51 +8,76 @@
 ; NOTE: this code will ignore duplicate permutations like ([1 1] [1 1]) if there were any
 
 (defn shift-it [lex i]
-  (let [rlex (reverse lex)
-        n (nth rlex i)
-        [head t] (split-at i rlex)
+  (let [ri (dec (- (count lex) i))
+        n (nth lex ri)
+        [h t] (split-at ri lex)
         tail (rest t)
-        spot (.indexOf tail (first (filter #(< % n) tail)))
-        [h2 t2] (split-at (inc spot) tail)
-        out-head (sort > (concat head h2))]
-      (when (not= spot -1)
-        (reverse (concat out-head [n] t2)))))
+        spot (.indexOf h (last (filter #(< % n) h)))
+        [h2 t2] (split-at spot h)
+        out-tail (sort (concat t2 tail))]
+      (when (not= -1 spot)
+        (concat h2 [n] out-tail))))
 
-(defn sub-next-lex [lex]
-  (first (keep #(shift-it lex %) (range (count lex)))))
+
+(defn first-shift [lex]
+  (some #(shift-it lex %) (range (count lex))))
+
+; (def shift-it (memoize shift-it))
+(def first-shift (memoize first-shift))
+
+(defn ranked-shift [lex]
+  (let [sorted (sort lex)
+        ranked (map #(.indexOf sorted %) lex)
+        shifted (first-shift ranked)]
+    (when shifted
+      (map #(nth sorted %) shifted))))
+
+(defn chop [lex i]
+  (let [[h t] (split-at (- (count lex) i) lex)
+        t2 (ranked-shift t)]
+    (when t2
+      (concat h t2))))
 
 (defn next-lex [lex]
-  (loop [i 1]
-    (let [[head tail] (split-at (- (count lex) i) lex)
-          new-tail (sub-next-lex tail)]
-      (if (> i (count lex))
-        (concat (rest lex) [(first lex)])
-        (if new-tail
-          (concat head new-tail)
-          (recur (inc i)))))))
+  (or (some #(chop lex %) (range 1 (inc (count lex))))
+      (concat (rest lex) [(first lex)])))
 
-; (defn chop [lex i]
-;   (let [[h t] (split-at (- (count lex) i) lex)
-;         t2 (sub-next-lex t)]
-;     (when t2 (concat h t2))))
+; (next-lex [0 1 2]) ;[0 2 1]
+; (next-lex [0 2 1]) ;[1 0 2]
+; (next-lex [1 0 2]) ;[1 2 0]
+; (next-lex [1 2 0]) ;[2 0 1]
+; (next-lex [2 0 1]) ;[2 1 0]
+
+(println '(0 2 1) "<-correct")
+(println (next-lex [0 1 2]))
+
+(println)
+
+
+; (println '(1 2 0) " : in")
+; (println (shift-it [1 2 0] 1) " : out")
+; (println '(2 0 1) "<-correct")
 ;
-; (defn next-lex [lex]
-;   (or (first (keep #(chop lex %) (range 1 (inc (count lex)))))
-;       (concat (rest lex) [(first lex)])))
+; (println)
+;
+(println '(0 1 2 3 4 5 6 9 7 8) "<-correct")
+(time (println (next-lex [0 1 2 3 4 5 6 8 9 7])))
+;
+(println '(0 1 3 9 8 4 6 7 2 5) "<-correct")
+(time (println (nth (iterate next-lex (range 10)) 10000)))
 
-(time (nth (iterate next-lex (range 10)) 10000))
+;
+; (assert (= (next-lex [0 1 2]) [0 2 1]))
+; (assert (= (next-lex [0 2 1]) [1 0 2]))
+; (assert (= (next-lex [1 0 2]) [1 2 0]))
+; (assert (= (next-lex [1 2 0]) [2 0 1]))
+; (assert (= (next-lex [2 0 1]) [2 1 0]))
+; (assert (= [0 1 2 3 4 5 6 9 7 8]
+;            (next-lex [0 1 2 3 4 5 6 8 9 7])))
+; (assert (= [0 1 3 9 8 4 6 7 2 5]
+;            (nth (iterate next-lex (range 10)) 10000)))
 
-(assert (= (next-lex [0 1 2]) [0 2 1]))
-(assert (= (next-lex [0 2 1]) [1 0 2]))
-(assert (= (next-lex [1 0 2]) [1 2 0]))
-(assert (= (next-lex [1 2 0]) [2 0 1]))
-(assert (= (next-lex [2 0 1]) [2 1 0]))
-(assert (= [0 1 2 3 4 5 6 9 7 8]
-           (next-lex [0 1 2 3 4 5 6 8 9 7])))
-(assert (= [0 1 3 9 8 4 6 7 2 5]
-           (nth (iterate next-lex (range 10)) 10000)))
-
-(println
+(println (time
   (nth (iterate next-lex (range 10)) 999999))
-
+)
 ; 2783915460
