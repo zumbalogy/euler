ed768652e0a9fa8fdfcc651f0784eb97ed67207a
zumbalogy <intrapostal@gmail.com>
2017-07-01 20:31:38 +0900

    p27 file touched

---
+++ 27/p27.clj
@@ -0,0 +1,18 @@
+; Euler discovered this remarkable quadratic formula:
+
+; n^2 + n + 41
+
+; The formula produces 40 primes for the consecutive integer values 0≤n≤39.
+; However, when n=40,40^2+40+41=40(40+1)+41 is divisible by 41,and when n=41,41^2+41+41 is divisible by 41.
+;
+; The formula n^2−79n+1601 produces 80 primes for the consecutive values 0≤n≤79.
+; The product of the coefficients, −79 and 1601, is −126479.
+;
+; Considering quadratics of the form:
+;
+; n^2+an+b, where |a|<1000 and |b|≤1000
+;
+; where |n| is the modulus/absolute value of n
+; e.g. |11|=11 and |−4|=4
+; Find the product of the coefficients, a and b, for the quadratic expression
+; that produces the maximum number of primes for consecutive values of n, starting with n=0.
