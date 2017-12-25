fad56fbd0ba601d26bd44094cd8c80eb08c2c7b5
zumbalogy <intrapostal@gmail.com>
2017-12-25 00:50:06 -0600

    clean

--- 07/p7.f90
+++ 07/p7.f90
@@ -8,13 +8,12 @@ program p7
 contains
 
   function nth_prime(n)
+    ! NOTE: does not work on 2, the first prime
     integer :: nth_prime
     integer :: n
     integer :: i, count
-
     i = 3
     count = 1
-
     do while (count < n)
        if (is_prime(i)) then
           count = count + 1
@@ -25,30 +24,20 @@ contains
   end function nth_prime
 
   function is_prime(n)
+    ! NOTE: does not work on 2, the only even prime
     logical :: is_prime
     integer :: n
     integer :: i, sq
     logical :: out
-
-    out = .true.
-    i = 3
+    out = mod(n, 2) /= 0
     sq = int(sqrt(n + 0.0))
-
-    if (0 == mod(n, 2)) then
-       out = .false.
-    end if
-
-    do while (and(out, i < sq + 1))
-       if (0 == mod(n, i)) then
-          out = .false.
-       end if
+    i = 3
+    do while (and(out, i <= sq))
+       out = mod(n, i) /= 0
        i = i + 2
     end do
-
     is_prime = out
-
   end function is_prime
 
 end program p7
-
 ! 104743
