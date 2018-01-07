434c045ede3df4187d5c05ca2c2648b5deaf4312
zumbalogy <intrapostal@gmail.com>
2018-01-07 16:34:05 -0600

    clean up comments

--- 102/p102.rb
+++ 102/p102.rb
@@ -1,17 +1,16 @@
 # coding: utf-8
-=begin
 
-Three distinct points are plotted on a Cartesian plane, for which -1000 ≤ x, y ≤ 1000, forming a triangle.
+# Three distinct points plotted on a Cartesian plane, for which -1000 ≤ x, y ≤ 1000, form a triangle.
 
-Consider the following two triangles:
-A(-340,495), B(-153,-910), C(835,-947)
-X(-175,41), Y(-421,-714), Z(574,-645)
+# Consider the following two triangles:
+# A(-340, 495), B(-153, -910), C(835, -947)
+# X(-175, 41), Y(-421, -714), Z(574, -645)
 
-It can be verified that triangle ABC contains the origin, whereas triangle XYZ does not.
+# It can be verified that triangle ABC contains the origin, whereas XYZ does not.
 
-Using triangles.txt (right click and 'Save Link/Target As...'), a 27K text file containing the co-ordinates of one thousand "random" triangles, find the number of triangles for which the interior contains the origin.
+# Using triangles.txt, a 27K text file containing the co-ordinates of one thousand "random" triangles,
+# find the number of triangles for which the interior contains the origin.
 
-=end
 
 triangles = File.readlines(File.dirname($0) + '/triangles.txt').map do |line|
   x1, y1, x2, y2, x3, y3 = line.split(',').map(&:to_i)
