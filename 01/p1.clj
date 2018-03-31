12abe995453342ec7f8dd309619bbe41de230edf
zumbalogy <intrapostal@gmail.com>
2018-03-30 23:56:46 -0500

    scheme and cleaner e1 for lisps

--- 01/p1.clj
+++ 01/p1.clj
@@ -1,14 +1,15 @@
-; The natural numbers below 10 that are multiples of 3 or 5 are 3, 5, 6 and 9.
-; The sum of these multiples is 23.
-; Find the sum of all the multiples of 3 or 5 below 1000.
+;; The natural numbers below 10 that are multiples of 3 or 5 are 3, 5, 6 and 9.
+;; The sum of these multiples is 23.
+;; Find the sum of all the multiples of 3 or 5 below 1000.
 
 (defn sum-under [input]
-  (* input (/ (inc input) 2)))
+  (* 1/2 input (inc input)))
 
 (defn sum-steps [step limit]
   (* step (sum-under (quot (dec limit) step))))
 
 (println
-  (- (+ (sum-steps 3 1000) (sum-steps 5 1000))
-     (sum-steps 15 1000)))
-; 233168
+ (- (+ (sum-steps 3 1000)
+       (sum-steps 5 1000))
+    (sum-steps 15 1000)))
+;; 233168
--- 01/p1.lisp
+++ 01/p1.lisp
@@ -1,11 +1,17 @@
-; The natural numbers below 10 that are multiples of 3 or 5 are 3, 5, 6 and 9.
-; The sum of these multiples is 23.
-; Find the sum of all the multiples of 3 or 5 below 1000.
+;; The natural numbers below 10 that are multiples of 3 or 5 are 3, 5, 6 and 9.
+;; The sum of these multiples is 23.
+;; Find the sum of all the multiples of 3 or 5 below 1000.
+
+(defun sum-under (limit)
+  (* 1/2 limit (+ 1 limit)))
+
+(defun sum-steps (step limit)
+  (* step (sum-under (floor (- limit 1) step))))
 
 (defvar output
-  (- (+ (reduce #'+ (loop for n from 0 below 1000 by 3 collect n))
-        (reduce #'+ (loop for n from 0 below 1000 by 5 collect n)))
-     (reduce #'+ (loop for n from 0 below 1000 by 15 collect n))))
+  (- (+ (sum-steps 3 1000)
+        (sum-steps 5 1000))
+     (sum-steps 15 1000)))
 
 (format t "~D~%" output)
-; 233168
+;; 233168
