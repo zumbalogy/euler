e7e43e4b4984b85a2d054b8d25909ee420e2f995
zumbalogy <intrapostal@gmail.com>
2018-03-30 23:56:46 -0500

    scheme and cleaner e1 for lisps

--- 01/p1.scm
+++ 01/p1.scm
@@ -1,5 +1,3 @@
-#lang racket
-
 ;; The natural numbers below 10 that are multiples of 3 or 5 are 3, 5, 6 and 9.
 ;; The sum of these multiples is 23.
 ;; Find the sum of all the multiples of 3 or 5 below 1000.
@@ -10,8 +8,9 @@
 (define (sum-steps step limit)
   (* step (sum-under (quotient (- limit 1) step))))
 
-(println
- (- (+ (sum-steps 3 1000)
-       (sum-steps 5 1000))
-    (sum-steps 15 1000)))
+(display (- (+ (sum-steps 3 1000)
+               (sum-steps 5 1000))
+            (sum-steps 15 1000)))
+
+(newline)
 ;; 233168
