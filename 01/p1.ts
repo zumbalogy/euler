0a4ac7c9fe61a76aea9437544b7c3f0282e2fade
zumbalogy <intrapostal@gmail.com>
2019-02-18 12:26:56 -0600

    p1 typescript

---
+++ 01/p1.ts
@@ -0,0 +1,14 @@
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+function sumUnder(input: number) {
+  return input * (input + 1) / 2
+}
+
+function sumSteps(step: number, limit: number) {
+  return step * sumUnder(Math.floor((limit - 1) / step))
+}
+
+console.log(
+  sumSteps(3, 1000) + sumSteps(5, 1000) - sumSteps(15, 1000)
+)
+// 233168
