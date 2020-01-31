3eda7cb45728ee1512be97390df36dd1b48e4cab
your name <foo@bar.com>
2020-01-31 17:15:07 +0900

    p2 forth

---
+++ 02/p2.fs
@@ -0,0 +1,19 @@
+: foo
+  0
+  1 2
+  begin
+    rot over + rot rot
+
+    over over + rot drop
+    over over + rot drop
+    over over + rot drop
+
+    dup 4000000 >
+  until
+  drop
+  drop
+;
+
+foo
+
+. cr
