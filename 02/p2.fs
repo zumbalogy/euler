99ff133fe86c5867025d6d7764f138bf191d351b
your name <foo@bar.com>
2020-02-01 19:45:31 +0900

    comments

--- 02/p2.fs
+++ 02/p2.fs
@@ -1,4 +1,14 @@
-: foo
+(
+  Each new term in the Fibonacci sequence is the sum of the previous 2 terms.
+  Starting with 1 and 2, the first 10 terms are:
+  1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+
+  Sum the even Fibonacci numbers under four million
+)
+
+: limit 4000000 ;
+
+: even_fibs_under_limit
   0
   1 2
   begin
@@ -8,12 +18,13 @@
     over over + rot drop
     over over + rot drop
 
-    dup 4000000 >
+    dup limit >
   until
   drop
   drop
 ;
 
-foo
+even_fibs_under_limit
 
 . cr
+( 4613732 )
