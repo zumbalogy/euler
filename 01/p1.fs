57e9eb9d717f1906592a1448f714cca80926e309
your name <foo@bar.com>
2020-02-01 19:45:31 +0900

    comments

--- 01/p1.fs
+++ 01/p1.fs
@@ -1,6 +1,10 @@
+(
+  The natural numbers below 10 that are multiples of 3 or 5 are 3, 5, 6 and 9.
+  The sum of these multiples is 23.
+  Find the sum of all the multiples of 3 or 5 below 1000.
+)
 
 : sum_under dup dup * + 2 / ;
-
 : sum_steps 1 - over / sum_under * ;
 
 3  1000 sum_steps
@@ -8,3 +12,4 @@
 15 1000 sum_steps -
 
 . cr
+( 233168 )
