95d002d54d358a565f6cbeffd6bb116600533fde
your name <foo@bar.com>
2020-02-03 13:39:06 +0900

    p3 in forth and comment syntax in other forth files

--- 01/p1.fs
+++ 01/p1.fs
@@ -1,8 +1,6 @@
-(
-  The natural numbers below 10 that are multiples of 3 or 5 are 3, 5, 6 and 9.
-  The sum of these multiples is 23.
-  Find the sum of all the multiples of 3 or 5 below 1000.
-)
+\ The natural numbers below 10 that are multiples of 3 or 5 are 3, 5, 6 and 9.
+\ The sum of these multiples is 23.
+\ Find the sum of all the multiples of 3 or 5 below 1000.
 
 : sum_under dup dup * + 2 / ;
 : sum_steps 1 - over / sum_under * ;
@@ -12,4 +10,4 @@
 15 1000 sum_steps -
 
 . cr
-( 233168 )
+\ 233168
--- 02/p2.fs
+++ 02/p2.fs
@@ -1,10 +1,8 @@
-(
-  Each new term in the Fibonacci sequence is the sum of the previous 2 terms.
-  Starting with 1 and 2, the first 10 terms are:
-  1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+\ Each new term in the Fibonacci sequence is the sum of the previous 2 terms.
+\ Starting with 1 and 2, the first 10 terms are:
+\ 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
 
-  Sum the even Fibonacci numbers under four million
-)
+\ Sum the even Fibonacci numbers under four million
 
 : limit 4000000 ;
 
@@ -27,4 +25,4 @@
 even_fibs_under_limit
 
 . cr
-( 4613732 )
+\ 4613732
