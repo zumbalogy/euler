e2ff03b340c5d29cc37c93f268cfafb86c9f098f
zumbalogy <zumbalogy@gmail.com>
2020-04-04 15:45:07 -0700

    cleaning run script

--- run.rb
+++ run.rb
@@ -9,26 +9,23 @@ runners = {
   rkt: -> f { `racket #{f}` },
 }
 
-dirs = (`ls`).scan(/\d+/)
-langs = runners.keys
+dirs = ARGV.select { |x| x[/\d+/] }.map { |x| x.rjust(3, '0') }
+exts = ARGV.select { |x| x[/\D+/] }.map { |x| x.to_sym }
 
-arg_dirs = ARGV.select { |x| x[/\d+/] }.map { |x| x.rjust(3, '0') }
-arg_langs = ARGV.select { |x| x[/\D+/] }
-
-dirs = arg_dirs if arg_dirs.any?
-langs = arg_langs.map(&:to_sym) if arg_langs.any?
+dirs = (`ls`).scan(/\d+/) unless dirs.any?
+exts = runners.keys unless exts.any?
 
 def get_key(dir)
   return unless File.file?("#{dir}/solution")
   File.read("#{dir}/solution").strip
 end
 
-def format(dir, lang, res, key, time)
+def format(dir, ext, res, key, time)
   str = dir.to_i.to_s.rjust(3)
   str += ' '
-  str += lang.to_s.ljust(6)
+  str += ext.to_s.ljust(6)
   str += '|'
-  str += res.rjust(10)
+  str += res.rjust(14)
   str += " | ." if key && key == res
   str += " | X (#{key})" if key && key != res
   str += " | ?" if key == nil
@@ -40,13 +37,13 @@ end
 dirs.each do |dir|
   num = dir.to_i.to_s
   key = get_key(dir)
-  langs.each do |lang|
-    file = "#{dir}/#{num}.#{lang}"
+  exts.each do |ext|
+    file = "#{dir}/#{num}.#{ext}"
     next unless File.file?(file)
     start = Time.now
-    res = runners[lang].call(file).to_f.to_s.sub!(/\.0+$/, '')
+    res = runners[ext].call(file).to_f.to_s.sub(/\.0+$/, '')
     time = Time.now - start
-    puts format(dir, lang, res, key, time)
+    puts format(dir, ext, res, key, time)
   end
-  puts if langs.length > 1
+  puts if exts.length > 1
 end
