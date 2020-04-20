74473a623cfb4eb53043f25f654d10ab4401d396
zumbalogy <zumbalogy@gmail.com>
2020-04-19 19:52:21 -0700

    formatting and clean of p7 clj

--- 007/7.clj
+++ 007/7.clj
@@ -1,11 +1,12 @@
 ; What is the 10 001st prime number?
 
 (defn prime? [n]
-  (if (even? n)
-    false
-    (not-any? #(zero? (rem n %))
-      (range 3 (inc (Math/sqrt n)) 2))))
-; gives true for 1, false for 2
+  ; gives true for 1, false for 2
+  (and (odd? n)
+       (not-any? #(zero? (rem n %))
+                 (range 3
+                        (inc (Math/sqrt n))
+                        2))))
 
 (println
   (nth (filter prime? (range)) 10000))
