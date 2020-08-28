3c19cebd71a89bafa97b9508abab483f1c76e3b1
_ <foo@bar.baz>
2020-08-27 22:18:44 -0700

    clojure solution working for 96

--- 096/96.rb
+++ 096/96.rb
@@ -132,7 +132,7 @@ def solve(cell_index = 0)
     Cells[cell_index] = number_guess
     solve(cell_index + 1)
     return if $score == 81
-    saved_cells.each_with_index { |c, i| Cells[i] = c }
+    saved_cells.each_with_index { |c, i| Cells[i] = c } # would reusing the clone here be faster?
     $score = saved_score
   end
 end
@@ -163,11 +163,11 @@ puzzles_chunks.each do |chunk|
 end
 
 
-# puts euler_output
+puts euler_output
 # 24702
 
-9.times do |i|
-  foo = i * 9
-  print Cells[foo...(foo + 9)].map { |x| cell_key[solution(x)] }
-  puts
-end
+# 9.times do |i|
+#   foo = i * 9
+#   print Cells[foo...(foo + 9)].map { |x| cell_key[solution(x)] }
+#   puts
+# end
