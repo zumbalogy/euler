b5313191709856da4bae35ff78cf509819abdeff
zumbalogy <foo@bar.baz>
2020-12-15 22:33:08 -0800

    note

--- 096/96.c
+++ 096/96.c
@@ -1,3 +1,16 @@
+// Su Doku (Japanese for 'number place') is the name of a popular puzzle.
+// Its origin is unclear but Leonhard Euler invented a similar, harder,
+// puzzle called Latin Squares.
+//
+// The goal of Su Doku is to replace the blanks (or zeros) in a 9x9 grid in
+// such that each row, column, and 3x3 box contains each of the digits 1-9.
+//
+// A proper Su Doku puzzle has a unique solution and is solvable by logic,
+// although it may be necessary to employ "guess and test" methods.
+//
+// The file sudoku.txt contains 50 Su Doku puzzles ranging in difficulty.
+// Find the sum of the 3-digit numbers in the top left corner of each solution.
+
 #include <stdio.h>
 
 int Cells[81];
